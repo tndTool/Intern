46911df80b08e16bbda6caaf0c3542574f8d01f6
I0ntdtn0I <19522362@gm.uit.edu.vn>
2023-05-17 10:02:08 +0700

    Update

---
+++ Topic 6/Bootstrap/client/src/App.tsx
@@ -0,0 +1,13 @@
+import Navigation from "./components/navigation";
+import Content from "./components/content";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ Topic 6/Bootstrap/client/src/components/navigation.tsx
@@ -0,0 +1,48 @@
+function Navigation() {
+  return (
+    <div className="Navigation">
+      <nav
+        className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top"
+        id="sideNav"
+      >
+        <a className="navbar-brand js-scroll-trigger" href="#page-top">
+          <span className="d-block d-lg-none">Clarence Taylor</span>
+          <span className="d-none d-lg-block">
+            <img
+              className="img-fluid img-profile rounded-circle mx-auto mb-2"
+              src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000"
+              alt="..."
+            />
+          </span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#experience">
+                Experience
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navigation;
